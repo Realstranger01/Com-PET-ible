76c47b1126d132e9824392deb19bfb5b6949048d
DrPeppr <md.purcaru@gmail.com>
2023-02-07 19:51:41 +0000

    result-js

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,28 +1,38 @@
-var breedNameEl = document.getElementById("breed-name");
-var breedTypeEl = document.getElementById("breed-type");
-var breedDescriptionEl = document.getElementById("breed-description");
-var userResultEl = document.getElementById("user-result");
+var dog = "Breed";
+var queryURL = "https://dogbreeddb.p.rapidapi.com/?search" + dog + "&apikey=breed";
 
-var query = breedNameEl.value;
-var url = '';
-
-userResultEl.addEventListener('click', function(event) {
-    event.preventDefault();
-    const breed = breedNameEl.value;
-    const type = breedTypeEl.value;
-    const description = breedDescriptionEl.value;
-    fetch('https://dogbreeddb.p.rapidapi.com/', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-})
+import axios from "axios";
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a9177b2819mshc7f150cf568bfc1p160e39jsnccdc2f6da663',
-		'X-RapidAPI-Host': 'dogbreeddb.p.rapidapi.com'
-	}
+  method: 'GET',
+  url: 'https://dogbreeddb.p.rapidapi.com/',
+  params: {id: '47'},
+  headers: {
+    'X-RapidAPI-Key': 'a9177b2819mshc7f150cf568bfc1p160e39jsnccdc2f6da663',
+    'X-RapidAPI-Host': 'dogbreeddb.p.rapidapi.com'
+  }
 };
 
-console.log(breedNameEl.value);
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
+
+$.ajax({
+    url: "https://dogbreeddb.p.rapidapi.com/?search=alaskan",
+    method: "GET"
+}).then(function(response) {
+    console.log(response[0]);
+
+var tr = $("<tr>");
+
+  var breedNameEl = $("<td>").text(response.Name);
+  var breedTypeEl = $("<td>").text(response.Type);
+  var breedDescriptionEl = $("<td>").text(response.Description);
+
+  tr.append(breedNameEl, breedTypeEl, breedDescriptionEl);
+  
+  $("tbody").append(tr);
+});
